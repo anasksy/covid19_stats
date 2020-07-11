eb14d7c87a5015e99f19d30e455d1608ce14b011
Anasksy <anasksz@gmx.de>
2020-07-11 22:54:15 +0200

    project demo deleted

---
+++ lib/Homescreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan[900],
+    );
+  }
+}
