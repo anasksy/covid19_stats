03e1c80fe324095bdcd25732e36bc9e63a7fcb2b
Charly <sleepycharlyy@hotmail.com>
2020-07-12 19:13:54 +0200

    utils.dart update

--- lib/utils.dart
+++ lib/utils.dart
@@ -2,7 +2,7 @@ import 'main.dart';
 
 class Utils {
   /* print debug message */
-  printDebug(String message) {
+  static void printDebug(String message) {
     if (debug_mode) {
       print("DEBUG: " + message);
     }
