7c1ecb073462b4f44988f71c2888b18a3bb28ca8
Anasksy <anasksz@gmx.de>
2020-07-12 19:03:31 +0200

    IconButtons added

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Homescreen extends StatelessWidget {
+class Homescreen extends StatefulWidget {
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,27 +15,24 @@ class Homescreen extends StatelessWidget {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(left: 14.4, top: 14.4),
-                  child: Icon(
-                    Icons.menu,
-                    color: Colors.white54,
-                    size: 35.0,
-                  ),
+                IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () {
+                    print("MENU BUTTON CLICKED");
+                  },
+                  color: Colors.white54,
+                  iconSize: 35.0,
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 259.2, top: 14.4),
-                  child: Icon(
-                    Icons.search,
-                    color: Colors.white54,
-                    size: 35.0,
-                  ),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    print("SEARCH BUTTON CLICKED");
+                  },
+                  color: Colors.white54,
+                  iconSize: 35.0,
                 ),
               ],
             ),
-            Container(
-              color: Colors.white,
-            )
           ],
         ),
       ),
