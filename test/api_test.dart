646e967c327233274cec34c4f1fa52c5322bd9b1
Charly <sleepycharlyy@hotmail.com>
2020-07-12 16:37:34 +0200

    setup sum testing files

---
+++ test/api_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+import 'package:covid19_mobileapp/api/api.dart';
+
+void main() {
+  test('title', () {
+    /* var result = function.validate('parse variable');
+     * expect(result, "what result u expect");
+     */
+  });
+}
