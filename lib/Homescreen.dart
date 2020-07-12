90537b0dccc41f6fda6991266e3939bfccf5d6bf
Anasksy <anasksz@gmx.de>
2020-07-12 18:23:36 +0200

    update - ui (icons)

--- lib/Homescreen.dart
+++ lib/Homescreen.dart
@@ -5,6 +5,35 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.cyan[900],
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: 14.4, top: 14.4),
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.white54,
+                    size: 35.0,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 259.2, top: 14.4),
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.white54,
+                    size: 35.0,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
