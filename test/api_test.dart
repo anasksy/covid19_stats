e56c3d08883097bd5470eee36f24ee2498b3ad51
Charly <sleepycharlyy@hotmail.com>
2020-07-13 21:10:41 +0200

    tests

--- test/api_test.dart
+++ test/api_test.dart
@@ -1,18 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:covid19_mobileapp/api/api.dart';
-import 'package:covid19_mobileapp/utils.dart';
-
-import 'dart:convert';
 
 void main() {
   test('load data (fetch json and convert it to usable data)', () {
     API.loadData();
     expect(API.loadData(), true);
-  });
-
-  test('testing json parsing', () async {
-    API.loadData();
-    var json_string = await API.json_string;
-    Map decoded = jsonDecode(json_string);
   });
 }
